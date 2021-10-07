fc2b23a362cceec8b23fa2a382eb41ebd311f5d4
cmccullison <cmccullison@vassar.edu>
2021-10-07 11:34:55 -0400

    created the Event.java file

---
+++ src/Event.java
@@ -0,0 +1,11 @@
+import java.sql.Time;
+import java.util.Date;
+
+public class Event
+{
+    Event (String name, Date eventDate, Time eventTime, int recurrence, String description, int upvoteDownvote, String posterUsername)
+    {
+
+    }
+
+}
