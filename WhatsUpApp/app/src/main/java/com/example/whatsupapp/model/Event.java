eb14c5086079968ba20558921253d18a68763470
John Lim <johnlim0043@gmail.com>
2021-11-08 20:33:36 -0500

    revised toString of event

--- WhatsUpApp/app/src/main/java/com/example/whatsupapp/model/Event.java
+++ WhatsUpApp/app/src/main/java/com/example/whatsupapp/model/Event.java
@@ -1,7 +1,6 @@
 package com.example.whatsupapp.model;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class Event {
     int upvoteDownvote = 0;
@@ -12,11 +11,12 @@ public class Event {
     String eventPoster;
     String eventLocation;
     String name;
-    String finalString = "";
+    String s = "";
     ArrayList<String> comments = new ArrayList<String>();
 
     Event (String name, String eDateAndTime, int recurrence, String description, String poster, String eLocation){
         this.eventName = name;
+        //separate date and time 
         this.eventDateAndTime = eDateAndTime;
         this.eventRecurrence = recurrence;
         this.eventPoster = poster;
@@ -26,33 +26,37 @@ public class Event {
 
     public String toString(){
         String score = Integer.toString(upvoteDownvote);
+        s += eventPoster +" posted: \n";
+        s += " On " + eventDateAndTime + " " + eventName + " at " + eventLocation;
+        s += "\n " + eventDescription;
+        return s;
         //String finalString2 = String.format("\n%d%s %t at %t posted by %s \n")
-        finalString = "\n";
-        finalString += score + "|";
-        finalString += eventName + " " + eventDateAndTime + " at " + eventLocation;
-        //finalString += eventName + " " + eventDateAndTime.getTime() + " at " + eventLocation;
-        finalString += new String(new char[score.length()]).replace("\0", " ") + "|" + " posted by " +  eventPoster; //.username;
-        finalString += "\n";
-        finalString += new String(new char[score.length()]).replace("\0", " ") + "|";
-        finalString += new String(new char[40 - score.length()]).replace("\0", "=");
-        finalString += "\n";
-        finalString += new String(new char[score.length()]).replace("\0", " ") + "|";
-        finalString += eventDescription;
-        finalString += "\n";
-        finalString += new String(new char[score.length()]).replace("\0", " ") + "|";
-        finalString += new String(new char[40 - score.length()]).replace("\0", "=");
-
-        for (String s : comments) {
-            finalString += new String(new char[40 - score.length()]).replace("\0", "-");
-            finalString += "\n";
-            finalString += new String(new char[score.length()]).replace("\0", " ") + "|";
-            finalString += s + "posted by " + eventPoster;//.username;
-            finalString += "\n";
-            finalString += new String(new char[score.length()]).replace("\0", " ") + "|";
-            finalString += new String(new char[40 - score.length()]).replace("\0", "-");
+//        s = "\n";
+//        s += score + "|";
+//        s += eventName + " " + eventDateAndTime + " at " + eventLocation;
+//        //finalString += eventName + " " + eventDateAndTime.getTime() + " at " + eventLocation;
+//        s += new String(new char[score.length()]).replace("\0", " ") + "|" + " posted by " +  eventPoster; //.username;
+//        s += "\n";
+//        s += new String(new char[score.length()]).replace("\0", " ") + "|";
+//        s += new String(new char[40 - score.length()]).replace("\0", "=");
+//        s += "\n";
+//        s += new String(new char[score.length()]).replace("\0", " ") + "|";
+//        s += eventDescription;
+//        s += "\n";
+//        s += new String(new char[score.length()]).replace("\0", " ") + "|";
+//        s += new String(new char[40 - score.length()]).replace("\0", "=");
+//
+//        for (String s : comments) {
+//            this.s += new String(new char[40 - score.length()]).replace("\0", "-");
+//            this.s += "\n";
+//            this.s += new String(new char[score.length()]).replace("\0", " ") + "|";
+//            this.s += s + "posted by " + eventPoster;//.username;
+//            this.s += "\n";
+//            this.s += new String(new char[score.length()]).replace("\0", " ") + "|";
+//            this.s += new String(new char[40 - score.length()]).replace("\0", "-");
         }
-        return finalString;
-    }
+
+
 
     public void addComment(String s){
         comments.add(s);
