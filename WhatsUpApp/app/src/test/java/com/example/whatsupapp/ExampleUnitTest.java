b41ff3b4ef0cbca04c5ee15c05cef363ff4dcd42
sgould <sgould@vassar.edu>
2021-11-18 20:28:36 -0500

    fixed errors

--- WhatsUpApp/app/src/test/java/com/example/whatsupapp/ExampleUnitTest.java
+++ WhatsUpApp/app/src/test/java/com/example/whatsupapp/ExampleUnitTest.java
@@ -4,19 +4,12 @@ import org.testng.annotations.Test;
 
 import static org.junit.Assert.*;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-
-    @org.junit.Rule
-    public ActivityScenarioRule<ControllerActivity> activityRule =
-            new ActivityScenarioRule<>(ControllerActivity.class);
-
 
     @Test
     public void addition_isCorrect() {
