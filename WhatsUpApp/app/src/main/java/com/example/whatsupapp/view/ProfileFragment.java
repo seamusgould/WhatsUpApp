d216e085b021f298c431525cb5af1b53977cfd51
John Lim <johnlim0043@gmail.com>
2021-12-08 00:53:52 -0500

    added binding to Profile Frag

--- WhatsUpApp/app/src/main/java/com/example/whatsupapp/view/ProfileFragment.java
+++ WhatsUpApp/app/src/main/java/com/example/whatsupapp/view/ProfileFragment.java
@@ -9,14 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.example.whatsupapp.databinding.FragmentProfileBinding;
 import com.example.whatsupapp.R;
+import com.example.whatsupapp.model.Username;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    FragmentProfileBinding binding;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -25,6 +28,8 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        
+//        this.binding.usernameText.setText(Username.getUsername());
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
